027a32dd50ebdbb43aee1904e0f60f7db43d0aea
Tuahta Ramadhani <59135441+taraid97exe@users.noreply.github.com>
2025-02-20 14:44:59 +0700

    update file screensaver

---
+++ Screensaver/Form1.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Dock = DockStyle.Fill
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1600, 900)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
+        BackColor = Color.Black
+        ClientSize = New Size(1600, 900)
+        Controls.Add(PictureBox1)
+        FormBorderStyle = FormBorderStyle.None
+        MinimizeBox = False
+        Name = "Form1"
+        ShowInTaskbar = False
+        Text = "Screensaver"
+        TopMost = True
+        WindowState = FormWindowState.Maximized
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Screensaver/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub ScreensaverForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Menampilkan animasi GIF pada PictureBox
+        PictureBox1.Image = My.Resources.Always_Lock_Your_Computer_01
+        ' Ganti "YourGif" dengan nama animasi GIF Anda di Resources
+        ' Atur properti form untuk fullscreen
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+        Me.TopMost = True
+
+        PictureBox1.Dock = DockStyle.Fill
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+    End Sub
+
+    ' Menutup screensaver saat ada aktivitas mouse atau keyboard
+    Private Sub ScreensaverForm_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        Me.Close()
+    End Sub
+
+    Private Sub ScreensaverForm_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        Me.Close()
+    End Sub
+
+    Private Sub ScreensaverForm_MouseClick(sender As Object, e As MouseEventArgs) Handles MyBase.MouseClick
+        Me.Close()
+    End Sub
+
+End Class
